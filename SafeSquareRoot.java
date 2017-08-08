5bf1d5f86d61b892886256048a7938df32952b82
Alexis Low <alexisthelow@gmail.com>
2017-08-08 15:38:58 -0600

    study drill

--- SafeSquareRoot.java
+++ SafeSquareRoot.java
@@ -4,8 +4,19 @@ public class SafeSquareRoot {
 	public static void main(String[] args) {
 		Scanner keyboard = new Scanner(System.in);
 		double x, y;
+		String reply;
+		String replyCheck = "YES!";
 		
-		System.out.print("Give me a number, and I'll find it's square root. (No negatives please!)");
+		
+		System.out.print("ARE YOU READY?! (please reply 'YES!'): ");
+		reply = keyboard.nextLine();
+		
+		while (! reply.equals(replyCheck)) {
+			System.out.print("C'mon.. I asked... ARE YOU READY?! (please reply 'YES!'): ");
+			reply = keyboard.nextLine();
+		}
+		
+		System.out.println("Give me a number, and I'll find it's square root. (No negatives please!)");
 		System.out.print("Your number: ");
 		x = keyboard.nextDouble();
 		
